780c56e11bf1f14dda3d7afdef8aba2cbc812319
dorivaldongoma <dorivaldongoma@gmail.com>
2023-06-14 20:09:42 +0100

    Funcionalidades Js/AJAX finalizadas

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// Elementos do DOM
+const $inputArquivos = document.querySelector("#inputArquivos"),
+    $btnEnviar = document.querySelector("#btnEnviar"),
+    $estado = document.querySelector("#estado");
+$btnEnviar.addEventListener("click", async () => {
+    const arquivosParaSubir = $inputArquivos.files;
+    if (arquivosParaSubir.length <= 0) {
+        // Se não houver arquivos, não continuaremos
+        return;
+    }
+    // Preparar os dados do formulário
+    const formData = new FormData();
+    // Adicionamos cada arquivo a "arquivos[]". Os colchetes são importantes
+    for (const arquivo of arquivosParaSubir) {
+        formData.append("arquivos[]", arquivo);
+    }
+    // Nós os enviamos
+    $estado.textContent = "Enviando arquivos...";
+    const respostaRaw = await fetch("../set/guardar.php", {
+        method: "POST",
+        body: formData,
+    });
+    const resposta = await respostaRaw.json();
+    // Você pode tratar a resposta como desejar
+    console.log({ resposta });
+    // Finalmente limpamos o campo
+    $inputArquivos.value = null;
+    $estado.textContent = "Arquivos enviados com sucesso";
+});
